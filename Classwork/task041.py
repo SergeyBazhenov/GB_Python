75962ee844d4359209f45f1304b4e0b0290c64bf
SergeyBazhenov <8618@mail.ru>
2023-01-29 19:38:22 +0500

    Калькулятор

--- Classwork/task041.py
+++ Classwork/task041.py
@@ -49,41 +49,74 @@
 
 # 2 действие
 
-n = '10 * 10 * 10'#"22 * 300 - 14 + 10 * 10 + 10 * 10"
+import os
 
-m = n.split()
-m2 = []
-
-print(m)
-
+os.system('cls')
 
 def calc(a, b, ch):
-    if ch == "+":
+    if ch == '+':
         return a + b
-    elif ch == "-":
+    elif ch == '-':
         return a - b
-    elif ch == "/":
+    elif ch == '/':
         return a / b
-    elif ch == "*":
+    elif ch == '*':
         return a * b
 
 
-# a = int(m[0])
-# c = m[1]
-# b = int(m[2])
+n = '10 * 10 * 10 / 22 * 300 - 14 + 10 * 10 + 10 * 10'
+input_list = n.split()
 
-result = int(m[0])
+first_prior = '()'
+second_prior = '*/'
+third_prior = '+-'
 
-for i in range(1, len(m) - 1, 2):
-    if m[i] == "*" or m[i] == "/":
-        result = calc(int(m[i - 1]), int(m[i + 1]), m[i])
-        m.append(result)
-                
+rpn = []
+stack = [n[1]]
+
+for i in range(len(input_list)):
+    if input_list[i].isdigit():
+        rpn.append(input_list[i])
+
+    elif input_list[i] in third_prior: 
+        while stack[-1] in third_prior or stack[-1] in second_prior:
+            rpn.append(stack[-1])
+            stack.pop()    
+        stack.append(input_list[i])
+
+    elif input_list[i] in second_prior:
+        if stack[-1] in second_prior:
+            rpn.append(stack[-1])
+            stack.pop()
+        stack.append(input_list[i])
+
+    elif input_list[i] == '(':
+        stack.append(input_list[i])
+
+    elif input_list[i] == ')':
+        rpn.append(stack[-1])
+        while stack[-1] != '(':
+            stack.pop()
+        stack.pop()       
+        
+    
+
+stack.reverse()
+stack.pop()
+
+rpn += stack
+
+result_list = []
+
+for i in range(len(rpn)):
+    
+    if rpn[i] in '+-*/':
+        b = float(result_list.pop())
+        a = float(result_list.pop())
+        result_list.append(calc(a, b, rpn[i]))
+    
     else:
-        m.append(m[i])
-        m.append(int(m[i + 1]))
+        result_list.append(rpn[i])
+   
 
-
-print(m[i], m[i + 1])
-print(m)
-print(result)
+print(f'Результат равен: {result_list[0]}')
